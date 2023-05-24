64de4713bdfbf95593bccee3a90c6ee9494a9e21
xianhc <apevolo@gmail.com>
2023-05-24 11:15:21 +0800

    [fix] 修改serilog文件输出路径

--- ApeVolo.Api/Middleware/SerilogMiddleware.cs
+++ ApeVolo.Api/Middleware/SerilogMiddleware.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using ApeVolo.Common.DI;
+using System.IO;
+using System.Text;
 using ApeVolo.Common.Global;
-using ApeVolo.Common.WebApp;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 using Serilog.Events;
@@ -12,8 +11,6 @@ namespace ApeVolo.Api.Middleware;
 
 public static class SerilogMiddleware
 {
-    private static readonly string SerilogPath = Environment.CurrentDirectory + "\\Logs\\{0}\\.log";
-
     private const string SerilogOutputTemplate =
         "{NewLine}时间:{Timestamp:yyyy-MM-dd HH:mm:ss.fff}{NewLine}所在类:{SourceContext}{NewLine}等级:{Level}{NewLine}信息:{Message}{NewLine}{Exception}";
 
@@ -29,37 +26,25 @@ public static class SerilogMiddleware
             logger.MinimumLevel.Override("Microsoft", LogEventLevel.Information);
             logger.MinimumLevel.Override("Default", LogEventLevel.Information);
             logger.MinimumLevel.Override("System", LogEventLevel.Information);
+
+            foreach (LogEventLevel logEvent in Enum.GetValues(typeof(LogEventLevel)))
+            {
+                logger.WriteTo.Logger(lg =>
+                    lg.Filter.ByIncludingOnly(p => p.Level == logEvent).WriteTo.Async(a =>
+                        a.File(Path.Combine(AppSettings.ContentRootPath, "Logs", logEvent.ToString(), ".log"),
+                            rollingInterval:
+                            RollingInterval.Day,
+                            outputTemplate:
+                            SerilogOutputTemplate, encoding:
+                            Encoding.UTF8)));
+            }
+
             if (AppSettings.IsDevelopment)
             {
                 //开发环境下输出日志到控制台
                 logger.WriteTo.Console();
             }
 
-            logger.WriteTo.Logger(lg =>
-                    lg.Filter.ByIncludingOnly(p => p.Level == LogEventLevel.Debug).WriteTo.Async(a =>
-                        a.File(string.Format(SerilogPath, LogEventLevel.Debug.ToString()),
-                            rollingInterval: RollingInterval.Day,
-                            outputTemplate: SerilogOutputTemplate)))
-                .WriteTo.Logger(lg =>
-                    lg.Filter.ByIncludingOnly(p => p.Level == LogEventLevel.Information).WriteTo.Async(a =>
-                        a.File(string.Format(SerilogPath, LogEventLevel.Information.ToString()),
-                            rollingInterval: RollingInterval.Day,
-                            outputTemplate: SerilogOutputTemplate)))
-                .WriteTo.Logger(lg =>
-                    lg.Filter.ByIncludingOnly(p => p.Level == LogEventLevel.Warning).WriteTo.Async(a =>
-                        a.File(string.Format(SerilogPath, LogEventLevel.Warning.ToString()),
-                            rollingInterval: RollingInterval.Day,
-                            outputTemplate: SerilogOutputTemplate)))
-                .WriteTo.Logger(lg =>
-                    lg.Filter.ByIncludingOnly(p => p.Level == LogEventLevel.Error).WriteTo.Async(a =>
-                        a.File(string.Format(SerilogPath, LogEventLevel.Error.ToString()),
-                            rollingInterval: RollingInterval.Day,
-                            outputTemplate: SerilogOutputTemplate)))
-                .WriteTo.Logger(lg =>
-                    lg.Filter.ByIncludingOnly(p => p.Level == LogEventLevel.Fatal).WriteTo.Async(a =>
-                        a.File(string.Format(SerilogPath, LogEventLevel.Fatal.ToString()),
-                            rollingInterval: RollingInterval.Day,
-                            outputTemplate: SerilogOutputTemplate)));
             //需要配置elasticsearch环境使用
             //docker run --name elasticsearch -d -e ES_JAVA_OPTS="-Xms512m -Xmx512m" -e "discovery.type=single-node" -p 9200:9200 -p 9300:9300 elasticsearch:7.5.0
             if (AppSettings.GetValue<bool>("Middleware", "Elasticsearch", "Enabled"))
